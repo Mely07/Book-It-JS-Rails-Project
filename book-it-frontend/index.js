ec9c72bba1a66cc75baf0a8d0a77087b633036f5
Mely07 <damely.tineo07@gmail.com>
2020-08-06 20:40:41 -0400

    Added close buttons with event listeners for all forms.

--- book-it-frontend/index.js
+++ book-it-frontend/index.js
@@ -13,7 +13,14 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   //USER
-  document.getElementById("submitNewUser").addEventListener("click", function (event) {
+  closeNewUser.addEventListener("click", (event) => {
+    event.preventDefault();
+    document.getElementById("userForm").hidden = !document.getElementById("userForm").hidden
+    document.getElementById("top").hidden = !document.getElementById("top").hidden;
+  });
+
+  //USER 
+  document.getElementById("submitNewUser").addEventListener("click", (event) => {
     document.getElementById("newBook").hidden = false;
     document.getElementById("newUser").hidden = true;
 
@@ -29,26 +36,31 @@ document.addEventListener("DOMContentLoaded", () => {
   //BOOK
   newBook.addEventListener("click", (event) => {
     event.preventDefault();
-    
+
     document.getElementById("bookForm").hidden = !document.getElementById("bookForm").hidden;
     document.getElementById("top").hidden = !document.getElementById("top").hidden;
   });
 
   //BOOK
-  document.getElementById("submitNewBook").addEventListener("click", function (event) {
+  closeNewBook.addEventListener("click", (event) => {
+    document.getElementById("bookForm").hidden = true;
+    document.getElementById("top").hidden = false;
+  });
+
+  //BOOK
+  document.getElementById("submitNewBook").addEventListener("click", (event) => {
     event.preventDefault();
     if (document.getElementById('title').value === "" || document.getElementById('author').value === "" || document.getElementById('subject').value === "" || document.getElementById('rating').value === "") {
       alert('Required fields: Title, Author, Subject, Rating');
     }
     else {
       createNewBook();
-      
     }
     document.getElementById("bookFormId").reset();
   });
 
   //COMMENT
-  document.getElementById("submitNewComment").addEventListener("click", function (event) {
+  document.getElementById("submitNewComment").addEventListener("click", (event) => {
     event.preventDefault();
     if (document.getElementById('body').value === "") {
       alert('Required field');
@@ -57,6 +69,12 @@ document.addEventListener("DOMContentLoaded", () => {
       createNewComment();
     }
     document.getElementById("commentFormId").reset();
+  });
+
+  //COMMENT
+  document.getElementById("closeNewComment").addEventListener("click", (event) => {
+    event.preventDefault();
+    document.getElementById("commentForm").hidden = !document.getElementById("commentForm").hidden;
   });
 
   //COMMENT
@@ -79,7 +97,7 @@ document.addEventListener("DOMContentLoaded", () => {
     fetchBooks(subject)
   })
 
-  document.getElementById("newComment").addEventListener("click", function (event) {
+  document.getElementById("newComment").addEventListener("click", (event) => {
     event.preventDefault();
     if (Object.keys(currentUser).length === 0) {
       alert('Please Complete User Form to Post');
@@ -90,174 +108,172 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-  //COMMENT
-  function createNewComment() {
-    const body = document.getElementById('body').value
-    const username = currentUser.username
-    const book_id = currentBookId
+//COMMENT
+function createNewComment() {
+  const body = document.getElementById('body').value
+  const username = currentUser.username
+  const book_id = currentBookId
 
-    let comment = {
-      body: body,
-      username: username,
-      book_id: book_id
-    }
+  let comment = {
+    body: body,
+    username: username,
+    book_id: book_id
+  }
 
-    let configObj = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify(comment)
-    }
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(comment)
+  }
 
-    fetch("http://localhost:3000/books/" + currentBookId + "/comments", configObj)
+  fetch("http://localhost:3000/books/" + currentBookId + "/comments", configObj)
 
-      .then(resp => (resp.json()))
-      .then(comment => {
-        //.log(comment)
-        let c = new Comment(comment.id, comment.body, comment.book_id, comment.username)
+    .then(resp => (resp.json()))
+    .then(comment => {
+      let c = new Comment(comment.id, comment.body, comment.book_id, comment.username)
+      c.renderComment();
+    })
+}
+
+//COMMENT
+function toggleComments(id) {
+  (event) => {
+    event.preventDefault()
+  };
+  currentBookId = id;
+  fetchComments(id);
+  closeAllComments();
+}
+
+//COMMENT
+function closeAllComments() {
+  document.getElementById("comments").innerHTML = '';
+  document.getElementById("bookComments").hidden = !document.getElementById("bookComments").hidden;
+  document.getElementById("books").hidden = !document.getElementById("books").hidden;
+  document.getElementById("top").hidden = !document.getElementById("top").hidden;
+  document.getElementById("dropDown").hidden = !document.getElementById("dropDown").hidden;
+
+}
+
+//COMMENT
+function fetchComments(id) {
+
+  document.getElementById("commentsList").innerHTML = '';
+  fetch("http://localhost:3000/books/" + id + "/comments")
+    .then(resp => resp.json())
+    .then(comments => {
+      for (const comment of comments) {
+        let c = new Comment(comment.id, comment.body, comment.book_id, comment.username);
         c.renderComment();
-      })
+      }
+    })
+}
+
+//BOOK
+function createNewBook() {
+  const title = document.getElementById('title').value.split(' ').map(w => w.substring(0, 1).toUpperCase() + w.substring(1)).join(' ');
+  const author = document.getElementById('author').value.split(' ').map(w => w.substring(0, 1).toUpperCase() + w.substring(1)).join(' ');
+  const publisher = document.getElementById('publisher').value
+  const subject = document.getElementById('subject').value
+  const review = document.getElementById('review').value
+  const rating = document.getElementById('rating').value
+
+  //title.required = true
+
+  let book = {
+    title: title,
+    author: author,
+    publisher: publisher,
+    subject: subject,
+    review: review,
+    rating: rating,
+    poster_username: currentUser.username,
+    poster_email: currentUser.email,
+    poster_grade: currentUser.grade,
+    likes: 0,
   }
 
-  //COMMENT
-  function toggleComments(id) {
-    currentBookId = id
-
-    closeAllComments();
-
-    fetchComments(id);
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(book)
   }
 
-  //COMMENT
-  function closeAllComments() {
-    document.getElementById("bookComments").hidden = !document.getElementById("bookComments").hidden;
-    document.getElementById("books").hidden = !document.getElementById("books").hidden;
-    document.getElementById("top").hidden = !document.getElementById("top").hidden;
-    document.getElementById("dropDown").hidden = !document.getElementById("dropDown").hidden;
-  }
+  fetch("http://localhost:3000/books", configObj)
+    .then(resp => (resp.json()))
+    .then(book => {
+      let b = new Book(book.id, book.title, book.author, book.publisher, book.subject, book.review, book.rating, book.poster_username, book.poster_email, book.poster_grade, book.likes, book.image)
+      b.renderBook();
+    })
+}
 
+//BOOK
+function fetchBooks(subject) {
+  document.getElementById("dropDown").hidden = false;
 
-  //COMMENT
-  function fetchComments(id) {
-    fetch("http://localhost:3000/books/" + id + "/comments")
-      .then(resp => resp.json())
-      .then(comments => {
-        console.log(comments)
-        for (const comment of comments) {
-          let c = new Comment(comment.id, comment.body, comment.book_id, comment.username);
-          c.renderComment();
+  document.getElementById("books").innerHTML = '';
+  fetch("http://localhost:3000/books")
+    .then(resp => resp.json())
+    .then(books => {
+      if (subject) {
+        let filteredBooks = books.filter(book => book.subject == subject);
+        console.log(filteredBooks)
+        filteredBooks.sort((a, b) => a.id - b.id);
+        for (const book of filteredBooks) {
+          let b = new Book(book.id, book.title, book.author, book.publisher, book.subject, book.review, book.rating, book.poster_username, book.poster_email, book.poster_grade, book.likes, book.image);
+          b.renderBook();
         }
-      })
-  }
-
-  //BOOK
-  function createNewBook() {
-    const title = document.getElementById('title').value.split(' ').map(w => w.substring(0,1).toUpperCase()+w.substring(1)).join(' ');
-    const author = document.getElementById('author').value.split(' ').map(w => w.substring(0,1).toUpperCase()+w.substring(1)).join(' ');
-    const publisher = document.getElementById('publisher').value
-    const subject = document.getElementById('subject').value
-    const review = document.getElementById('review').value
-    const rating = document.getElementById('rating').value
-    const image = document.getElementById('image').value
-
-    title.required = true
-
-    let book = {
-      title: title,
-      author: author,
-      publisher: publisher,
-      subject: subject,
-      review: review,
-      rating: rating,
-      poster_username: currentUser.username,
-      poster_email: currentUser.email,
-      poster_grade: currentUser.grade,
-      likes: 0,
-      image: image
-    }
-
-    let configObj = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify(book)
-    }
-
-
-    fetch("http://localhost:3000/books", configObj)
-      .then(resp => (resp.json()))
-      .then(book => {
-        let b = new Book(book.id, book.title, book.author, book.publisher, book.subject, book.review, book.rating, book.poster_username, book.poster_email, book.poster_grade, book.likes, book.image)
-        b.renderBook();
-      })
-  }
-
-
-  //BOOK
-  function fetchBooks(subject) {
-    document.getElementById("dropDown").hidden = false;
-
-    document.getElementById("books").innerHTML = '';
-    fetch("http://localhost:3000/books")
-      .then(resp => resp.json())
-      .then(books => {
-        if (subject) {
-          let filteredBooks = books.filter(book => book.subject == subject);
-          console.log(filteredBooks)
-          filteredBooks.sort((a, b) => a.id - b.id);
-          for (const book of filteredBooks) {
-            let b = new Book(book.id, book.title, book.author, book.publisher, book.subject, book.review, book.rating, book.poster_username, book.poster_email, book.poster_grade, book.likes, book.image);
-            b.renderBook();
-          }
+      }
+      else {
+        books.sort((a, b) => a.id - b.id);
+        for (const book of books) {
+          let b = new Book(book.id, book.title, book.author, book.publisher, book.subject, book.review, book.rating, book.poster_username, book.poster_email, book.poster_grade, book.likes, book.image);
+          b.renderBook();
         }
-        else {
-          books.sort((a, b) => a.id - b.id);
-          for (const book of books) {
-            let b = new Book(book.id, book.title, book.author, book.publisher, book.subject, book.review, book.rating, book.poster_username, book.poster_email, book.poster_grade, book.likes, book.image);
-            b.renderBook();
-          }
-        }
-      })
+      }
+    })
+}
+
+//BOOK
+function likeBook(id) {
+  fetch("http://localhost:3000/books/" + id)
+    .then(resp => resp.json())
+    .then(json => {
+      let formData = {
+        "likes": json.likes + 1
+      };
+
+      let configObj = {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+
+      fetch("http://localhost:3000/books/" + id, configObj)
+        .then(() => fetchBooks())
+    })
+}
+
+//USER
+function assembleCurrentUser() {
+  currentUser.username = document.getElementById('username').value
+  currentUser.email = document.getElementById('email').value
+  currentUser.grade = document.getElementById('grade').value
+
+  if (currentUser) {
+    document.getElementById("interName").hidden = !document.getElementById("interName").hidden;
+    document.getElementById("interName").innerHTML = ', ' + currentUser.username;
   }
 
-  //BOOK
-  function likeBook(id) {
-    fetch("http://localhost:3000/books/" + id)
-      .then(resp => resp.json())
-      .then(json => {
-        let formData = {
-          "likes": json.likes + 1
-        };
-
-        let configObj = {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-          },
-          body: JSON.stringify(formData)
-        };
-
-        fetch("http://localhost:3000/books/" + id, configObj)
-          .then(() => fetchBooks())
-      })
-  }
-
-  //USER
-  function assembleCurrentUser() {
-    currentUser.username = document.getElementById('username').value
-    currentUser.email = document.getElementById('email').value
-    currentUser.grade = document.getElementById('grade').value
-
-    if (currentUser) {
-      document.getElementById("interName").hidden = !document.getElementById("interName").hidden;
-      document.getElementById("interName").innerHTML = ', ' + currentUser.username;
-    }
-    
-    document.getElementById("userForm").hidden = true;
-    document.getElementById("top").hidden = false;
-  }
+  document.getElementById("userForm").hidden = true;
+  document.getElementById("top").hidden = false;
+}
--- book-it-frontend/src/book.js
+++ book-it-frontend/src/book.js
@@ -1,5 +1,5 @@
 class Book {
-    constructor (id, title, author, publisher, subject, review, rating, poster_username, poster_email, poster_grade, likes, image){
+    constructor(id, title, author, publisher, subject, review, rating, poster_username, poster_email, poster_grade, likes, image) {
         this.id = id;
         this.title = title;
         this.author = author;
@@ -21,11 +21,10 @@ class Book {
         this.template = this.template.replace("RATING", this.rating);
         this.template = this.template.replace("LIKES", this.likes);
         this.template = this.template.replace(/BOOKID/g, this.id);
-        this.template = this.template.replace("IMAGE", this.image );
-        
-        this.template = this.template.replace("USERNAME", this.poster_username); 
-        this.template = this.template.replace("EMAIL", this.poster_email); 
-        this.template = this.template.replace("GRADE", this.poster_grade);  
+
+        this.template = this.template.replace("USERNAME", this.poster_username);
+        this.template = this.template.replace("EMAIL", this.poster_email);
+        this.template = this.template.replace("GRADE", this.poster_grade);
 
         document.getElementById("books").innerHTML += this.template;
 
@@ -34,29 +33,25 @@ class Book {
     }
 
     template =
-    "<div class='col-md-4' style='padding: 8px'>"+
-    "  <div class='card shadow-sm'>"+
-    "    <div class='card-body overflow-auto' style='padding: 15px; height: 14.5rem;'>"+
-    "      <h4 class='mb-0'>TITLE</h4>"+
-    " <br>"+
-    "      <strong class='d-inline-block mb-1 text-success'>AUTHOR</strong>"+
-    "      <div class='text-muted'>Rating: RATING/5</div>"+
-    "      <p class='card-text mb-1 mt-3 font-italic'>REVIEW</p>"+
-    "      <div class='text-muted mb-2'>Posted By: USERNAME, EMAIL, GRADE grade</div>"+
-    "      <div class='d-flex justify-content-between align-items-center mt-2'>"+
-    "        <div>"+
-    "          <button id='comments' type='button' class='btn btn-sm btn-outline-secondary'onclick='toggleComments(BOOKID)'>Comments</button>"+
-    "        </div>"+
-    "        <div class='text-right'>LIKES <svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-heart' fill='currentColor' onclick='likeBook(BOOKID)'"+
-    "          xmlns='http://www.w3.org/2000/svg'>"+
-    "          <path fill-rule='evenodd'"+
-    "            d='M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z' />"+
-    "        </svg></div>"+
-    "      </div>"+
-    "    </div>"+
-    "  </div>"+
-    "</div>";
-
-
-
+        "<div class='col-md-4' style='padding: 8px'>" +
+        "  <div class='card shadow-sm'>" +
+        "    <div class='card-body overflow-auto' style='padding: 15px; height: 15rem;'>" +
+        "      <h4 class='mb-0'>TITLE</h4>" +
+        "      <strong class='d-inline-block mb-1 text-success'>AUTHOR</strong>" +
+        "      <div class='text-muted'>Rating: RATING/5</div>" +
+        "      <p class='card-text mb-1 mt-3 font-italic'>REVIEW</p>" +
+        "      <div class='text-muted mb-2'>Posted By: USERNAME, EMAIL, GRADE grade</div>" +
+        "      <div class='d-flex justify-content-between align-items-center mt-2'>" +
+        "        <div>" +
+        "          <button id='comments' type='button' class='btn btn-sm btn-outline-secondary'onclick='toggleComments(BOOKID)'>Comments</button>" +
+        "        </div>" +
+        "        <div class='text-right'>LIKES <svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-heart' fill='currentColor' onclick='likeBook(BOOKID)'" +
+        "          xmlns='http://www.w3.org/2000/svg'>" +
+        "          <path fill-rule='evenodd'" +
+        "            d='M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z' />" +
+        "        </svg></div>" +
+        "      </div>" +
+        "    </div>" +
+        "  </div>" +
+        "</div>";
 }
